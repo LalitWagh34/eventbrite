8c6ba66284185b2cce1af72c0f3304e6065b5773
unknown <lalitwagh2804@gmail.com>
2024-04-16 19:26:16 +0530

    Added the events

---
+++ eventbrite/app/(root)/events/[id]/update/page.tsx
@@ -0,0 +1,23 @@
+import EventForm from '@/components/shared/EventForm'
+import { auth } from '@clerk/nextjs'
+import React from 'react'
+
+const CreateEvents = () => {
+    const {sessionClaims} = auth();
+
+    const userId = sessionClaims?.userId as string;
+
+  return (
+    <>
+        <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py'>
+            <h3 className='wrapper h3-bold text-center sm:text-left'> Create Events</h3>
+        </section>
+        <div className='wrapper my-8'>
+            <EventForm userId={userId} type="Update"/>
+        </div>
+    </>
+ 
+  )
+}
+
+export default CreateEvents
