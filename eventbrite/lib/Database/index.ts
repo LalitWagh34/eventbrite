0903b787379f51b02570f59cbfdfbef2341b83a3
unknown <lalitwagh2804@gmail.com>
2024-04-16 19:54:51 +0530

    upDated

--- eventbrite/lib/Database/index.ts
+++ eventbrite/lib/Database/index.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-const MONGODB_URL =process.env.MONGODB_URL;
+const MONGODB_URI =process.env.MONGODB_URI;
 
 let cached  = (global as any).mongoose || {conn:null , promise:null};
 
 export const connectToDataBase = async ()=>{
     if(cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error('MONGODB_URL is missing');
+    if(!MONGODB_URI) throw new Error('MONGODB_URL is missing');
 
-    cached.promise=cached.promise || mongoose.connect(MONGODB_URL ,{
+    cached.promise=cached.promise || mongoose.connect(MONGODB_URI ,{
         dbName:'eventbrite',
         bufferCommands:false,
     })
